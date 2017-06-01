4e983465515d2a0c261b4f7e6843d3ef47f4d284
RobertRedford <clarksjake@gmail.com>
2017-05-31 17:10:52 -0700

    Python 65 Drill
    Add Drill 65 files

---
+++ Python 65 Files/functions.py
@@ -0,0 +1,46 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+import shutil
+import os
+import datetime
+
+def browseSrcFiles(self):
+    self.srcfolder.delete(0, 'end')
+    directory = filedialog.askdirectory()
+    print (directory)
+    self.srcfolder.insert(0, directory+'/')
+
+def browseDstFiles(self):
+    self.dstfolder.delete(0, 'end')
+    directory = filedialog.askdirectory()
+    print (directory)
+    self.dstfolder.insert(0, directory+'/')
+
+
+now = datetime.datetime.now()
+print(now)
+before = now - datetime.timedelta(hours = 24)
+print (before)
+
+def moveFiles(self):
+    source = self.srcfolder.get()
+    files = os.listdir(source)
+    destination = self.dstfolder.get()
+    for f in files:
+        if f.endswith(".txt"):
+            modtime = datetime.datetime.fromtimestamp(os.path.getmtime(source+f))
+            if modtime < before:
+                src = source+f
+                dst = destination+f
+                shutil.move(src, dst)
+
+def onClear(self):
+    self.srcfolder.delete(0, 'end')
+    self.dstfolder.delete(0, 'end')          
+
+      
+
+        
+
+
